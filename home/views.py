ab5fdd23c1fd8a49491ea72072d31d738e0f074b
mark-obeirne <markobeirne00@gmail.com>
2020-11-28 14:16:09 +0000

    Add best seller to index view
    - Get best selling game details and return to template

--- home/views.py
+++ home/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
+from products.models import Product
 
 
 def index(request):
     """ A view to display the index page """
-    return render(request, "home/index.html")
+    best_seller = Product.objects.order_by('quantity_sold')[0]
+    context = {
+        "best_seller": best_seller
+    }
+    return render(request, "home/index.html", context)
