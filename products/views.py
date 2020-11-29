1bae8e52312f2319ae50c333962dececefffd283
mark-obeirne <markobeirne00@gmail.com>
2020-11-29 13:14:34 +0000

    Update all_products view
    - Add variables for sort and direction
    - Create if statement for sort and direction in request
    - Update products ordering based on sortkey
    - Create variable to pass current sorting to template

--- products/views.py
+++ products/views.py
@@ -1,10 +1,31 @@
 from django.shortcuts import render
 from .models import Product
+from django.db.models.functions import Lower
 
 
 def all_products(request):
     products = Product.objects.all()
+    sort = None
+    direction = None
+
+    if "sort" in request.GET:
+        sortkey = request.GET["sort"]
+        sort = sortkey
+        if "sortkey" == "name":
+            sortkey = "lower_name"
+            products = products.annotate(lower_name=Lower("name"))
+
+        if "direction" in request.GET:
+            direction = request.GET["direction"]
+            if direction == "desc":
+                sortkey = f"-{sortkey}"
+        products = products.order_by(sortkey)
+
+    current_sorting = f"{sort}_{direction}"
+
     context = {
-        "products": products
+        "products": products,
+        "current_sorting": current_sorting,
     }
+
     return render(request, "products/products.html", context)
