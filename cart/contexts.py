a41cb5b81e69b12549467e97a1d6164f70f268ef
mark-obeirne <markobeirne00@gmail.com>
2020-12-03 11:38:25 +0000

    Create context processor for use across apps
    - Create context processor to outline cart items, product count, total, and
    calculate grand total taking delivery into account

---
+++ cart/contexts.py
@@ -0,0 +1,28 @@
+from decimal import decimal
+from django.conf import settings
+
+def cart_contents(request):
+    cart_items = []
+    total = 0
+    product_count = 0
+
+    if total < settings.FREE_DELIVERY_THRESHOLD:
+        delivery = settings.STANDARD_DELIVERY_FEE
+        free_delivery_remainder = settings.FREE_DELIVERY_THRESHOLD - total
+    else:
+        delivery = 0
+        free_delivery_remainder = 0
+    
+    grand_total = total + delivery
+
+    context = {
+        "cart_items": cart_items,
+        "total": total,
+        "product_count": product_count,
+        "delivery": delivery,
+        "free_delivery_remainder": free_delivery_remainder,
+        "free_delivery_threshold": settings.FREE_DELIVERY_THRESHOLD,
+        "grand_total": grand_total,
+    }
+
+    return context
