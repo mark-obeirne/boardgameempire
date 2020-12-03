057c18f3cda0b1bab08bdbae117a0776c1dd0c9e
mark-obeirne <markobeirne00@gmail.com>
2020-12-03 17:05:55 +0000

    Create add_to_cart view
    - Create view to enable user to add product to cart
    - Update view to add 1 to product_id value if product is already in cart

--- cart/views.py
+++ cart/views.py
@@ -1,5 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 
 def view_cart(request):
-    return render(request, 'cart/cart.html')
+    """ View items currently in cart """
+    return render(request, "cart/cart.html")
+
+
+def add_to_cart(request, product_id):
+    """ Add a product to the cart """
+    cart = request.session.get("cart", {})
+    redirect_url = request.POST.get('redirect_url')
+
+    if product_id in list(cart.keys()):
+        cart[product_id] += 1
+    else:
+        cart[product_id] = 1
+
+    request.session["cart"] = cart
+    print(cart)
+    return redirect(redirect_url)
