f6cdd9acb2e7843409bdbc662da7437b26926542
mark-obeirne <markobeirne00@gmail.com>
2020-12-06 10:29:58 +0000

    Create checkout view
    Create view to return order form to template
    Update view to ensure that there are items in the user's cart

---
+++ checkout/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render, redirect, reverse
+from django.contrib import messages
+from .forms import OrderForm
+
+
+def checkout(request):
+    cart = request.session.get("cart", {})
+    if not cart:
+        messages.error(request, "Your cart is currently empty")
+        return redirect(reverse('products'))
+    order_form = OrderForm()
+    context = {
+        "order_form": order_form
+    }
+    return render(request, "checkout/checkout.html", context)
