7718151a830177f8e08214ca2e856cdf1f8c4cbc
mark-obeirne <markobeirne00@gmail.com>
2020-12-09 19:41:36 +0000

    Add gift_purchase to fields

--- checkout/forms.py
+++ checkout/forms.py
@@ -5,7 +5,7 @@ from .models import Order
 class OrderForm(forms.ModelForm):
     class Meta:
         model = Order
-        fields = ("full_name", "email", "street_address1", "street_address2", "town_or_city", "county_or_state", "postcode", "country", "billing_full_name", "billing_street_address1", "billing_street_address2", "billing_town_or_city", "billing_county_or_state", "billing_country",)
+        fields = ("full_name", "email", "street_address1", "street_address2", "town_or_city", "county_or_state", "postcode", "country", "billing_full_name", "billing_street_address1", "billing_street_address2", "billing_town_or_city", "billing_county_or_state", "billing_country", "gift_purchase")
 
         def __init__(self, *args, **kwargs):
             """ 
