4d4257ce8ad46c5eda6746d654c4940f4c70f396
mark-obeirne <markobeirne00@gmail.com>
2020-12-15 21:49:51 +0000

    Update profile view to get wishlist
    - Get and return user's wishlist as part of context

--- profiles/views.py
+++ profiles/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from .models import UserProfile
+from wishlists.models import Wishlist
+from products.models import Product
 from .forms import UserProfileForm
 from checkout.models import Order
 from django.contrib import messages
@@ -8,6 +10,8 @@ from django.contrib import messages
 def profile(request):
     """ Display the user's profile """
     profile = get_object_or_404(UserProfile, user=request.user)
+    wishlist, created = Wishlist.objects.get_or_create(user_profile=profile)
+    wishlisted_products = Product.objects.filter(wishlist__user_profile=profile)
 
     if request.method == "POST":
         form = UserProfileForm(request.POST, instance=profile)
@@ -25,6 +29,7 @@ def profile(request):
         "profile": profile,
         "form": form,
         "orders": orders,
+        "wishlisted_products": wishlisted_products,
     }
 
     return render(request, "profiles/profile.html", context)
