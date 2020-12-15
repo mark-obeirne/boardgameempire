bc647482c3d287b9637fc68aee14fa9fe471fbc2
mark-obeirne <markobeirne00@gmail.com>
2020-12-15 19:27:31 +0000

    Update media_url and static_url
    - Add slash to the end of both urls

--- boardgame_empire/settings.py
+++ boardgame_empire/settings.py
@@ -194,8 +194,8 @@ if 'USE_AWS' in os.environ:
     MEDIAFILES_LOCATION = 'media'
 
     # Override static and media URLs
-    STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}'
-    MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}'
+    STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}/'
+    MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}/'
 
 # Delivery Cost Settings
 FREE_DELIVERY_THRESHOLD = 50
