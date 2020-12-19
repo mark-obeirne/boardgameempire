748ad840a4e00745737e4f384976fe1d2fd5ba30
mark-obeirne <markobeirne00@gmail.com>
2020-12-19 13:55:21 +0000

    Add contact urls
    - Add contact url
    - Add contact_success url

--- about/urls.py
+++ about/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('', views.about_us, name="about_us"),
     path('loyalty/', views.loyalty, name="loyalty"),
     path('returns/', views.returns, name="returns"),
+    path('contact/', views.contact_us, name="contact_us"),
+    path('contact-success/', views.contact_success, name="contact_success"),
 ]
