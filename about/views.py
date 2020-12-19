1ea35c3f282f2f9edc0ee0ec6eef34f1c9090cc7
mark-obeirne <markobeirne00@gmail.com>
2020-12-19 13:58:39 +0000

    Create contact views
    - Create get request view to return form
    - Add if statement for POST method
    - Check if form is valid
    - Send email if form is valid
    - Create basic contact_success view

--- about/views.py
+++ about/views.py
@@ -1,4 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
+from .forms import ContactForm
+from django.core.mail import send_mail, BadHeaderError
+from django.contrib import messages
+from django.http import HttpResponse, HttpResponseRedirect
 
 
 def about_us(request):
@@ -11,3 +15,32 @@ def loyalty(request):
 
 def returns(request):
     return render(request, "about/returns.html")
+
+
+def contact_us(request):
+    form = ContactForm()
+
+    if request.method == "POST":
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            subject = form.cleaned_data["subject"]
+            from_email = form.cleaned_data["from_email"]
+            message = form.cleaned_data["message"]
+            try:
+                send_mail(subject, message, from_email, ["boardgameempire@example.com"])
+                messages.success(request, "Thank you for your email! We will be in touch.")
+            except BadHeaderError:
+                return HttpResponse("Invalid header found.")
+            return redirect("contact_success")
+        else:
+            messages.error(request, "Sorry, couldn't send your email. Please ensure form is valid")
+
+    context = {
+        "form": form,
+    }
+
+    return render(request, "about/contact.html", context)
+
+
+def contact_success(request):
+    return render(request, "about/contact-success.html")
