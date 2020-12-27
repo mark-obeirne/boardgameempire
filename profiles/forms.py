2c391562967b43147a8f9a607be800dbe66251ed
mark-obeirne <markobeirne00@gmail.com>
2020-12-27 21:29:00 +0000

    Add comments

--- profiles/forms.py
+++ profiles/forms.py
@@ -9,7 +9,8 @@ class UserProfileForm(forms.ModelForm):
 
     def __init__(self, *args, **kwargs):
         """
-        Add placeholders and classes, remove auto-generated labels and set autofocus
+        Add placeholders and classes, remove auto-generated labels and
+        set autofocus
         """
         super().__init__(*args, **kwargs)
         placeholders = {
