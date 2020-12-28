eda39d9ac765c7cd145e54f7f3166a37c0bb0fb0
mark-obeirne <markobeirne00@gmail.com>
2020-12-28 11:01:28 +0000

    Create function to handle manual points entry
    - Move functions from checkout.html to file
    - Create function to check if user enters a valid points amount to use and
    update points amount if less than 0, more than the points tally available to
    them or is not a multiple of 50

---
+++ checkout/static/checkout/js/checkout.js
@@ -0,0 +1,125 @@
+// Materialize Initialisation
+document.addEventListener('DOMContentLoaded', function() {
+        var elems = document.querySelectorAll('select');
+        var instances = M.FormSelect.init(elems, {});
+    });
+
+
+// Element Selectors
+    
+    const billingCheckbox = document.querySelector("#same-billing-shipping")
+    const pointsToUse = document.querySelector("#id_points_used")
+
+// Functions
+
+    // Show or hide billing details section based on checkbox to indicate that billing details match delivery details
+    function toggleBillingDetails() {
+        const billingDetails = document.querySelector(".billing-details")
+        if (billingCheckbox.checked) {
+                billingDetails.classList.add("none");
+                autofillBilling();
+            } else {
+                billingDetails.classList.remove("none");
+                autofillBilling();
+            }
+    }
+
+    // Autofill billing details if user checks box to indicate that billing details match delivery details
+    function autofillBilling(){
+            if (billingCheckbox.checked) {
+                form.billing_full_name.value = form.full_name.value
+                form.billing_street_address1.value = form.street_address1.value
+                form.billing_street_address2.value = form.street_address2.value
+                form.billing_town_or_city.value = form.town_or_city.value
+                form.billing_county_or_state.value = form.county_or_state.value
+                form.billing_country.value = form.country.value
+            } else {
+                form.billing_full_name.value = ""
+                form.billing_street_address1.value = ""
+                form.billing_street_address2.value = ""
+                form.billing_town_or_city.value = ""
+                form.billing_county_or_state.value = ""
+                form.billing_country.value = ""
+            }
+        }
+
+    // Update grand total as user opts to use loyalty points
+        
+        function updateGrandTotal() {
+            // Get cart total and points user has opted to use
+            const cartTotal = document.querySelector(".cart-total")
+            let cartTotalText = cartTotal.innerText
+            const regex = /[€\s]/g
+            let totalToPay = parseFloat(cartTotalText.replace(regex, ""))
+            const userInputPoints = pointsToUse.value
+            const pointsCashEquivalent = userInputPoints / 500
+
+            // Get delivery cost
+            let deliveryCost = 0 
+            const deliveryText = document.querySelector(".delivery-cost")
+            if (deliveryText.innerText === "€6.00") {
+                deliveryCost = 6
+            }
+
+            // Calculate cart total remaining
+            const cartTotalRemaining = (totalToPay - pointsCashEquivalent + deliveryCost).toFixed(2)
+            const cartTotalRemainingText = "€" + cartTotalRemaining.toString()
+
+            // Change grand total text values
+            let grandTotal = document.querySelectorAll(".grand-total")
+            grandTotal.forEach(total => total.innerText = cartTotalRemainingText)
+        }
+
+
+        // Update points available to use number select limits
+        
+        function setPointsAvailable() {
+            const pointsToUse = document.querySelector("#id_points_used")
+            if (pointsToUse) {
+                const pointsAvailable = document.querySelector(".user-loyalty-points").textContent
+                pointsToUse.setAttribute("min", "0");
+                pointsToUse.setAttribute("max", pointsAvailable);
+                pointsToUse.setAttribute("step", "50");
+            }
+        }
+
+
+        // Check loyalty points entered are valid if user enters number manually
+        function checkValidPointsUsed() {
+            const pointsAvailable = parseInt(document.querySelector(".user-loyalty-points").textContent)
+            const userEnteredPoints = parseInt(pointsToUse.value)
+
+            // Check if user enters a points value that is less than 0 or greater than loyalty points earned
+            if (userEnteredPoints < 0) {
+                pointsToUse.value = 0;
+            } else if (userEnteredPoints > pointsAvailable) {
+                // Check if pointsAvailable is divisible by 50 and remove remainder to use a multiple of 50 if not
+                const remainder = pointsAvailable % 50
+                if (remainder) {
+                    pointsToUse.value = pointsAvailable - remainder
+                }
+            }
+            
+            // Check if entered points value is divisible by 50 and remove remainder if not
+            if (userEnteredPoints % 50 != 0) {
+                const excessPoints = userEnteredPoints % 50
+                pointsToUse.value = userEnteredPoints - excessPoints
+            }
+
+        }
+
+// Event Listeners
+    
+    // Show or hide billing details section based on checkbox to indicate that they match delivery details
+    billingCheckbox.addEventListener("change", toggleBillingDetails)
+
+    // Check loyalty points entered are valid if user enters number manually
+    pointsToUse.addEventListener("change", checkValidPointsUsed)
+
+    // Update grand total as user opts to use loyalty points
+    if (pointsToUse) {
+        pointsToUse.addEventListener("change", updateGrandTotal)}
+
+    
+    // Update points available to use number select limits
+    document.addEventListener('DOMContentLoaded', setPointsAvailable)
