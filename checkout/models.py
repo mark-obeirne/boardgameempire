50ab14b788fa3c4f25f37d16ea9049c9e70869a6
mark-obeirne <markobeirne00@gmail.com>
2020-12-30 21:33:04 +0000

    Fix highlighted errors
    - Correct line length errors by breaking field details and comments over
    multiple lines
    - Remove null=True from CharFields

--- checkout/models.py
+++ checkout/models.py
@@ -10,30 +10,43 @@ from profiles.models import UserProfile
 
 class Order(models.Model):
     order_number = models.CharField(max_length=32, null=False, editable=False)
-    user_profile = models.ForeignKey(UserProfile, on_delete=models.SET_NULL, null=True, blank=True, related_name="orders")
+    user_profile = models.ForeignKey(
+        UserProfile, on_delete=models.SET_NULL,
+        null=True, blank=True, related_name="orders")
     full_name = models.CharField(max_length=60, null=False, blank=False)
     email = models.EmailField(max_length=254, null=False, blank=False)
     street_address1 = models.CharField(max_length=80, null=False, blank=False)
-    street_address2 = models.CharField(max_length=80, null=True, blank=True)
+    street_address2 = models.CharField(max_length=80, blank=True)
     town_or_city = models.CharField(max_length=40, null=False, blank=False)
-    county_or_state = models.CharField(max_length=40, null=True, blank=True)
-    postcode = models.CharField(max_length=20, null=True, blank=True)
+    county_or_state = models.CharField(max_length=40, blank=True)
+    postcode = models.CharField(max_length=20, blank=True)
     country = CountryField(blank_label="Country *", null=False, blank=False)
     order_date = models.DateTimeField(auto_now_add=True)
-    points_earned = models.IntegerField(null=False, blank=False, editable=False, default=0)
+    points_earned = models.IntegerField(
+        null=False, blank=False, editable=False, default=0)
     points_used = models.IntegerField(null=False, blank=False, default=0)
-    delivery_cost = models.DecimalField(max_digits=4, decimal_places=2, null=False, default=0)
-    order_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
-    grand_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
-    billing_full_name = models.CharField(max_length=60, null=False, blank=False)
-    billing_street_address1 = models.CharField(max_length=80, null=False, blank=False)
-    billing_street_address2 = models.CharField(max_length=80, null=True, blank=True)
-    billing_town_or_city = models.CharField(max_length=40, null=False, blank=False)
-    billing_county_or_state = models.CharField(max_length=40, null=True, blank=True)
-    billing_country = CountryField(blank_label="Country *", null=False, blank=False)
+    delivery_cost = models.DecimalField(
+        max_digits=4, decimal_places=2, null=False, default=0)
+    order_total = models.DecimalField(
+        max_digits=10, decimal_places=2, null=False, default=0)
+    grand_total = models.DecimalField(
+        max_digits=10, decimal_places=2, null=False, default=0)
+    billing_full_name = models.CharField(
+        max_length=60, null=False, blank=False)
+    billing_street_address1 = models.CharField(
+        max_length=80, null=False, blank=False)
+    billing_street_address2 = models.CharField(
+        max_length=80, blank=True)
+    billing_town_or_city = models.CharField(
+        max_length=40, null=False, blank=False)
+    billing_county_or_state = models.CharField(
+        max_length=40, blank=True)
+    billing_country = CountryField(
+        blank_label="Country *", null=False, blank=False)
     gift_purchase = models.BooleanField(default=False, null=True, blank=True)
     original_cart = models.TextField(null=False, blank=False, default='')
-    stripe_pid = models.CharField(max_length=254, null=False, blank=False, default='')
+    stripe_pid = models.CharField(
+        max_length=254, null=False, blank=False, default='')
 
     def __str__(self):
         return self.order_number
@@ -43,14 +56,21 @@ class Order(models.Model):
         return uuid.uuid4().hex.upper()
 
     def save(self, *args, **kwargs):
-        """ Override the original save method to set the order number if not already set """
+        """
+        Override the original save method to set the order number if not
+        already set
+        """
         if not self.order_number:
             self.order_number = self.generate_order_number()
         super().save(*args, **kwargs)
 
     def update_grand_total(self):
-        """ Update grand total each time a line item is added, taking delivery costs into account """
-        self.order_total = self.lineitems.aggregate(Sum("lineitem_total"))["lineitem_total__sum"] or 0
+        """
+        Update grand total each time a line item is added,
+        taking delivery costs into account
+        """
+        self.order_total = self.lineitems.aggregate(
+            Sum("lineitem_total"))["lineitem_total__sum"] or 0
         if self.order_total < settings.FREE_DELIVERY_THRESHOLD:
             self.delivery_cost = settings.STANDARD_DELIVERY_FEE
         else:
@@ -60,22 +80,33 @@ class Order(models.Model):
 
     def update_points_earned(self):
         """ Update points earned on an order each time a line item is added """
-        self.points_earned = self.lineitems.aggregate(Sum("lineitem_points_earned"))["lineitem_points_earned__sum"] or 0
+        self.points_earned = self.lineitems.aggregate(
+            Sum("lineitem_points_earned"))["lineitem_points_earned__sum"] or 0
         self.save()
 
 
 class OrderLineItem(models.Model):
-    order = models.ForeignKey(Order, null=False, blank=False, on_delete=models.CASCADE, related_name="lineitems")
-    product = models.ForeignKey(Product, null=False, blank=False, on_delete=models.CASCADE)
+    order = models.ForeignKey(
+        Order, null=False, blank=False,
+        on_delete=models.CASCADE, related_name="lineitems")
+    product = models.ForeignKey(
+        Product, null=False, blank=False,
+        on_delete=models.CASCADE)
     quantity = models.IntegerField(null=False, blank=False, default=0)
-    lineitem_total = models.DecimalField(max_digits=6, decimal_places=2, null=False, blank=False, editable=False)
-    lineitem_points_earned = models.IntegerField(null=False, blank=False, default=0)
+    lineitem_total = models.DecimalField(
+        max_digits=6, decimal_places=2, null=False,
+        blank=False, editable=False)
+    lineitem_points_earned = models.IntegerField(
+        null=False, blank=False, default=0)
 
     def __str__(self):
         return f"SKU {self.product.sku} on order {self.order.order_number}"
 
     def save(self, *args, **kwargs):
-        """ Override original save method to set the lineitem total and the loyalty points earned """
+        """
+        Override original save method to set the lineitem total and the loyalty
+        points earned
+        """
         if self.product.on_sale:
             self.lineitem_total = self.product.sale_price * self.quantity
         else:
